51a1971a6f8b955f441839a5201a4eb6e759e5f2
emilssolmanis <emils.solmanis@gmail.com>
2012-08-28 01:25:29 +0300

    problem 26

--- project_euler.py
+++ project_euler.py
@@ -1,4 +1,4 @@
-import math
+import math, fractions
 
 # #################### PRIVATE PROPERTY NO TOUCHING ##############################
 
@@ -183,6 +183,34 @@ def __lex_permutations(elem_set):
                 permutations.append([elem] + perm)
 
     return permutations
+
+def __order(a, n):
+    """
+    Returns the order of a modulo n.
+    """
+    if fractions.gcd(a, n) != 1:
+        raise ValueError("GCD of a and n is not 1")
+
+    i = 1
+    while a**i % n != 1:
+        i += 1
+    return i
+
+def __reciprocal_length(denominator):
+    """
+    Returns the length of the recurrent part in decimal notation for a fraction 1 / denominator.
+    """
+    while denominator % 2 == 0:
+        denominator //= 2
+    
+    while denominator % 5 == 0:
+        denominator //= 5
+
+    # means it consisted of only 2 and 5 in the first place, it is terminal
+    if denominator == 1:
+        return 0
+    else:
+        return __order(10, denominator)
 
 # ############################## PUBLIC TOUCHY TOUCHY ##############################
 
@@ -789,3 +817,30 @@ def problem_25():
     for i, elem in enumerate(fibonacci()):
         if len(str(elem)) >= 1000:
             return i + 1 + 1
+
+def problem_26():
+    """
+    A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with
+    denominators 2 to 10 are given:
+
+    ^1/[2]  =  0.5        
+    ^1/[3]  =  0.(3)      
+    ^1/[4]  =  0.25       
+    ^1/[5]  =  0.2        
+    ^1/[6]  =  0.1(6)     
+    ^1/[7]  =  0.(142857) 
+    ^1/[8]  =  0.125      
+    ^1/[9]  =  0.(1)      
+    ^1/[10] =  0.1        
+    
+    Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that ^1/[7] has a
+    6-digit recurring cycle.
+
+    Find the value of d < 1000 for which ^1/[d] contains the longest recurring cycle in its decimal
+    fraction part.    
+    """
+    # Every rational number is either a recurrent or terminating decimal. The ones with a denominator
+    # consisting of only 2 and 5 are terminating. Everything else is recurrent.
+    rl = [__reciprocal_length(i) for i in range(1, 1000)]
+    m = max(rl)
+    return rl.index(m) + 1
