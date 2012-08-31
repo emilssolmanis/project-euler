bd18c932f0db820fe56640dbf4209d24d7cb6580
emilssolmanis <emils.solmanis@gmail.com>
2012-08-31 11:08:04 +0300

    added single problem timing

--- time_all.sh
+++ time_all.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-for (( problem=1 ; problem < 44 ; problem++ )); 
-do
-    elapsed=$(/usr/bin/time -f '%E' python3 -c "import project_euler; project_euler.problem_$problem()" 2>&1)
-    echo "$problem $elapsed"
-done
+if [ -z $@ ];
+then
+    for (( problem=1 ; problem < 44 ; problem++ )); 
+    do
+        elapsed=$(/usr/bin/time -f '%E' python3 -c "import project_euler; project_euler.problem_$problem()" 2>&1)
+        echo "$problem $elapsed"
+    done
+else
+    elapsed=$(/usr/bin/time -f '%E' python3 -c "import project_euler; project_euler.problem_$@()" 2>&1)
+    echo "$@ $elapsed"
+fi
