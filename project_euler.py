eaed6599a5a8c01a3be542872764b823f8e8a881
emilssolmanis <emils.solmanis@gmail.com>
2012-09-07 15:15:20 +0300

    problem 46

--- project_euler.py
+++ project_euler.py
@@ -1559,3 +1559,32 @@ def problem_45():
         if __is_pent_number(num) and __is_hex_number(num):
             return num
         i += 1
+
+def problem_46():
+    """
+    It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a square.
+
+    9 = 7 + 2*1²
+    15 = 7 + 2*2²
+    21 = 3 + 2*3²
+    25 = 7 + 2*3²
+    27 = 19 + 2*2²
+    33 = 31 + 2*1²
+
+    It turns out that the conjecture was false.
+
+    What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
+    """
+    prime_list = eratosthenes(10**5)
+    i = 3
+    while i < prime_list[-1]**2:
+        if not __is_prime(i, prime_list):
+            found = False
+            for j in range(1, math.floor(math.sqrt(i // 2)) + 1):
+                if __is_prime(i - 2 * j**2, prime_list):
+#                    print("{} = {} + 2 * {}²".format(i, i - 2 * j**2, j))
+                    found = True
+                    break
+            if not found:
+                return i
+        i += 2
