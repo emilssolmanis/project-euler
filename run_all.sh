0a7b3764b29fdd9f6748134b240bbff386608b62
emilssolmanis <emils.solmanis@gmail.com>
2012-09-08 15:30:50 +0300

    run_all parameter added

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-for (( problem=1 ; problem < 44 ; problem++ )); 
-do
-    python3 -c "import project_euler; print($problem, project_euler.problem_$problem())"
-done
+if [ -z $@ ];
+then
+    problems=$(grep problem_ project_euler.py | egrep -o '[[:digit:]]*' | sort -n | tail -n1)
+    for (( problem=1 ; problem <= $problems ; problem++ )); 
+    do
+        answer=$(python3 -c "import project_euler; print(project_euler.problem_$problem())" 2>&1)
+        echo "$problem $answer"
+    done
+else
+    answer=$(python3 -c "import project_euler; print(project_euler.problem_$@())" 2>&1)
+    echo "$@ $answer"
+fi
