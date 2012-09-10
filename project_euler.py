06600e0ef7aeb4f383ccccf2b3e646d41ce3f688
emilssolmanis <emils.solmanis@gmail.com>
2012-09-10 12:01:31 +0300

    problem 56

--- project_euler.py
+++ project_euler.py
@@ -2097,3 +2097,18 @@ def problem_55():
                 break
         res += lychrael
     return res
+
+def problem_56():
+    """
+    A googol (10^100) is a massive number: one followed by one-hundred zeros; 100^100 is almost 
+    unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits
+    in each number is only 1.
+
+    Considering natural numbers of the form, a^b, where a, b < 100, what is the maximum digital sum?
+    """
+    res = 0
+    for i in range(1, 100):
+        for j in range(1, 100):
+            res = max(res, sum(int(k) for k in str(i**j)))
+    
+    return res
