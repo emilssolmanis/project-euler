063c09e8861dd09abbdbcc6ff4856258e0492217
emilssolmanis <emils.solmanis@gmail.com>
2012-09-10 15:43:09 +0300

    problem 59

--- project_euler.py
+++ project_euler.py
@@ -2188,3 +2188,52 @@ def problem_58():
         i += 1
 
     return 2 * i - 1
+
+def problem_59(filename="problem_59.dat"):
+    """
+    Each character on a computer is assigned a unique code and the preferred standard is ASCII
+    (American Standard Code for Information Interchange). For example, uppercase A = 65, asterisk
+    (*) = 42, and lowercase k = 107.
+
+    A modern encryption method is to take a text file, convert the bytes to ASCII, then XOR each 
+    byte with a given value, taken from a secret key. The advantage with the XOR function is that 
+    using the same encryption key on the cipher text, restores the plain text; for example, 
+    65 XOR 42 = 107, then 107 XOR 42 = 65.
+
+    For unbreakable encryption, the key is the same length as the plain text message, and the key 
+    is made up of random bytes. The user would keep the encrypted message and the encryption key in
+    different locations, and without both "halves", it is impossible to decrypt the message.
+
+    Unfortunately, this method is impractical for most users, so the modified method is to use a 
+    password as a key. If the password is shorter than the message, which is likely, the key is
+    repeated cyclically throughout the message. The balance for this method is using a sufficiently 
+    long password key for security, but short enough to be memorable.
+
+    Your task has been made easy, as the encryption key consists of three lower case characters. 
+    Using cipher1.txt (right click and 'Save Link/Target As...'), a file containing the encrypted
+    ASCII codes, and the knowledge that the plain text must contain common English words, decrypt 
+    the message and find the sum of the ASCII values in the original text.
+    """
+    def __decrypt(cypher, key):
+        msg = []
+        for idx, char in enumerate(cypher):
+            msg.append(char ^ ord(key[idx % len(key)]))
+        return "".join(chr(i) for i in msg)
+
+
+    def __crack(cypher):
+        for a in range(ord('a'), ord('z') + 1):
+            for b in range(ord('a'), ord('z') + 1):
+                for c in range(ord('a'), ord('z') + 1):
+                    key = "".join([chr(a), chr(b), chr(c)])
+                    msg = __decrypt(cypher, key)
+                    if "the" in msg and " a " in msg:
+                        return msg
+
+    cypher = None
+    with open(filename, "r") as f:
+        cypher = [int(i) for i in f.readline().split(",")]
+    
+    msg = __crack(cypher)
+    print("Message: \n{}".format(msg))
+    return sum(ord(char) for char in msg)
